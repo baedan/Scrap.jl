e44b7d11a431ccd895b21934126b7584011f6a12
Documenter.jl <documenter@juliadocs.github.io>
2022-08-05 02:37:14 +0000

    build based on c19f597

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Scrap","category":"page"},{"location":"#Scrap","page":"Home","title":"Scrap","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Scrap.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Scrap]","category":"page"}]
+}
