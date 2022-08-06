616f1795bbe8def1ca725b43d2c316f6605d71b0
baedan <comely.typists0x@icloud.com>
2022-08-06 11:18:09 +0800

    fixed test

--- test/Effects/ApplyStatusEffect.jl
+++ test/Effects/ApplyStatusEffect.jl
@@ -1,3 +1,3 @@
 @testset "ApplyStatusEffect" begin
-    @test ApplyStatusEffect(WeakStatus('h', Ability("thing", AfterAttackTrigger(Scrap.PlayerTriggeredBy()), AllOfEffect(Vector{AllOfEffect}()))), "h") isa AddStatusEffect
+    @test ApplyStatusEffect(WeakStatus('h', Ability("thing", AfterAttackTrigger(Scrap.PlayerTriggeredBy()), AllOfEffect(Vector{AllOfEffect}()))), "h") isa ApplyStatusEffect
 end
