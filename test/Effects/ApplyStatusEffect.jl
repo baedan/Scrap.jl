34efe9dfe2eb2bd259af2c0af056b408ad4668f4
baedan <comely.typists0x@icloud.com>
2022-08-06 10:38:55 +0800

    added test

---
+++ test/Effects/ApplyStatusEffect.jl
@@ -0,0 +1,3 @@
+@testset "ApplyStatusEffect" begin
+    @test ApplyStatusEffect(WeakStatus('h', Ability("thing", AfterAttackTrigger(Scrap.PlayerTriggeredBy()), AllOfEffect(Vector{AllOfEffect}()))), "h") isa AddStatusEffect
+end
